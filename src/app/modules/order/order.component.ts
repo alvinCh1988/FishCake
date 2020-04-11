5add839db919f9e69faebe303364ae126e99518b
is-Yao <51728170+yao19881003@users.noreply.github.com>
2020-04-11 10:34:44 +0800

    order update

--- src/app/modules/order/order.component.ts
+++ src/app/modules/order/order.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
+  public orderList = [
+    { name: '原味', price: 45, quantity: 0, amount: 0, color: 'chocolate' },
+    { name: '巧克力', price: 50, quantity: 0, amount: 0, color: 'black' },
+    { name: '草莓', price: 50, quantity: 0, amount: 0, color: 'red' },
+    { name: '抹茶', price: 50, quantity: 0, amount: 0, color: 'green' },
+    { name: '黑糖', price: 50, quantity: 0, amount: 0, color: 'Brown' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
